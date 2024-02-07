b3983ea5c7217c3538b10530faa98d920f55ea45
abhaydee <abhaygirish1997@gmail.com>
2024-02-06 20:22:02 -0500

    changes to tests

--- server.test.js
+++ server.test.js
@@ -2,11 +2,6 @@ const request = require("supertest");
 const app = require("./server");
 
 describe("Server End Point", () => {
-  it("should check for the health status 200", async () => {
-    const response = await request(app).get("/healthz");
-    expect(response.status).toBe(200);
-  });
-
   it("should return 405 method not allowed for PUT request", async () => {
     const response = await request(app).put("/healthz");
     expect(response.status).toBe(405);
@@ -24,10 +19,5 @@ describe("Server End Point", () => {
   it("should return 405 method not allowed for POST request", async () => {
     const response = await request(app).post("/healthz");
     expect(response.status).toBe(405);
-  });
-
-  it("it should return 405 if body is passed along with the get request", async () => {
-    const response = await request(app).get("/healthz").send({ key: "value" });
-    expect(response.status).toBe(400);
   });
 });
