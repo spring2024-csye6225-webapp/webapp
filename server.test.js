82b828820cc337d7ecda4651a26a46ae3ab03886
Abhay Deshpande <abhaygirish1997@gmail.com>
2024-02-07 20:28:29 -0500

    Feature/crud (#1)
    * adding workflow file
    
    * crud operations
    
    * changes to workflow file
    
    * changes to tests
    
    * finishing workflow
    
    * editing workflow v2 for finish build
    
    * fixing the server infinite run time issue
    
    * upgrading node version
    
    * checking email validation
    
    * Feature/crud (#1)
    
    * adding workflow file
    
    * crud operations
    
    * changes to workflow file
    
    * changes to tests
    
    * finishing workflow
    
    * editing workflow v2 for finish build
    
    * fixing the server infinite run time issue
    
    * upgrading node version
    
    * checking email validation
    
    * updates to readme :
    
    * Feature/crud (#2)
    
    * adding workflow file
    
    * crud operations
    
    * changes to workflow file
    
    * changes to tests
    
    * finishing workflow
    
    * editing workflow v2 for finish build
    
    * fixing the server infinite run time issue
    
    * upgrading node version
    
    * checking email validation
    
    * updates to readme :
    
    * readme changes
    
    * workflow setup
    
    * Feature/crud (#3)
    
    * adding workflow file
    
    * crud operations
    
    * changes to workflow file
    
    * changes to tests
    
    * finishing workflow
    
    * editing workflow v2 for finish build
    
    * fixing the server infinite run time issue
    
    * upgrading node version
    
    * checking email validation
    
    * updates to readme :
    
    * readme changes
    
    * workflow setup
    
    * changes to readme v2

---
+++ server.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const { app, startServer } = require("./server");
+
+describe("Server End Point", () => {
+  it("should return 405 method not allowed for PUT request", async () => {
+    const response = await request(app).put("/healthz");
+    expect(response.status).toBe(405);
+  });
+
+  it("should return 405 method not allowed for DELETE request", async () => {
+    const response = await request(app).delete("/healthz");
+    expect(response.status).toBe(405);
+  });
+
+  it("should return 405 method not allowed for PATCH request", async () => {
+    const response = await request(app).patch("/healthz");
+    expect(response.status).toBe(405);
+  });
+  it("should return 405 method not allowed for POST request", async () => {
+    const response = await request(app).post("/healthz");
+    expect(response.status).toBe(405);
+  });
+
+  afterAll(() => {
+    const server = startServer();
+    server.close();
+  });
+});
