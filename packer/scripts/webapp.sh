c8b21c5f4de3863f2423e94fa57c093335b8f361
abhaydee <abhaygirish1997@gmail.com>
2024-02-21 20:12:36 -0500

    commiting packer changes

--- packer/scripts/webapp.sh
+++ packer/scripts/webapp.sh
@@ -10,7 +10,6 @@ sudo -u postgres psql -U postgres -c "CREATE USER abhaydeshpande WITH PASSWORD '
 sudo -u postgres psql -U postgres -c "CREATE DATABASE cloudusers;"
 sudo -u postgres psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE cloudusers to abhaydeshpande;"
 
-
 sudo sed -i 's/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            password/g' /var/lib/pgsql/data/pg_hba.conf
 
 sudo sed -i 's/host    all             all             ::1\/128                 ident/host    all             all             ::1\/128                 password/g' /var/lib/pgsql/data/pg_hba.conf
@@ -23,40 +22,26 @@ echo "Node.js and npm Versions"
 node -v
 npm -v
 
-
 echo "+-------------------------------------------------------------+"
 echo "|                    UNZIP WEBAPP                             |"
 echo "+-------------------------------------------------------------+"
 sudo yum install -y unzip
 
+echo "Unzipping the zip folder"
+sudo unzip -o /tmp/webapp-new.zip -d /tmp/
 
-echo "existing files"
- cd /tmp
+sleep 1m
 
-echo "logging the moved files to vm"
-
+echo "Unzipping completed"
 ls
-
-echo "Unzip the zip folder"
-sudo unzip -o /tmp/webapp-new.zip
-
-
-echo "zipped"
-echo "current directory"
-pwd
-ls 
 echo "Copy webapp-new.zip to user home directory"
-sudo cp -r webapp-new /opt/csye6225
-sudo cp -r webapp-new/packer/scripts/webapp.service /opt/csye6225
+sudo cp -r /tmp/webapp-new /opt/csye6225
+sudo cp -r /tmp/webapp-new/packer/scripts/webapp.service /etc/systemd/system/webapp.service
+echo "printing current directory"
+pwd
 
-echo "logging files here"
-cd /opt/csye6225
-
+echo "listing current directory items"
 ls
-
-
-echo "logging files before changing permissions"
-ls 
 
 echo "+-------------------------------------------------------------+"
 echo "|                    Setup csye6225 group                     |"
@@ -64,58 +49,27 @@ echo "+-------------------------------------------------------------+"
 sudo groupadd csye6225
 sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/csye6225 -m csye6225
 
-echo "after changing permissions"
-
-ls
-
-echo "Check webapp-new in the  directory"
-ls
-ls -ld /opt/csye6225
-sudo chmod -R 777 /opt/csye6225
-
-
-echo "Check if the webapp-new exists"
-ls 
-
-echo "+-------------------------------------------------------------+"
-echo "|                    Install Node Modules                     |"
-echo "+-------------------------------------------------------------+"
-echo "install node modules"
-sudo npm install
-
-echo "+-------------------------------------------------------------+"
-echo "|                    Setup webapp.service                     |"
-echo "+-------------------------------------------------------------+"
-echo "Copy webapp.service to /lib/systemd/system"
-echo "logging current files strcuture" 
-ls
-echo "copy"
-
-echo "node location"
-which node
-
-sudo cp webapp.service /lib/systemd/system
-
 echo "+-------------------------------------------------------------+"
 echo "|                    Setup new user permissions               |"
 echo "+-------------------------------------------------------------+"
-echo "Get the home directory of the user"
-echo ~csye6225
-echo "Display permissions of user directory"
-ls -la /opt/csye6225
-
 echo "Change permissions of webapp-new"
 sudo chown -R csye6225:csye6225 /opt/csye6225/
 sudo chmod -R 777 /opt/csye6225/
 
-echo "Display permissions of user directory after changes"
-ls -la /opt/csye6225
+echo " guess which directory I am in"
+ls
+
+cd /opt/csye6225
+
+echo "current directory before npm install"
+pwd
+
+npm install
+
 
 echo "+-------------------------------------------------------------+"
 echo "|                    Setup Systemd                            |"
 echo "+-------------------------------------------------------------+"
-
-
 sudo systemctl start webapp.service
 sudo systemctl status webapp.service
 sudo systemctl enable webapp.service
