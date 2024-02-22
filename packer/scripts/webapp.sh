fa872bc893270f08d98ee03b4cedb72269e67f81
abhaydee <abhaygirish1997@gmail.com>
2024-02-21 20:57:51 -0500

    changes to dir

--- packer/scripts/webapp.sh
+++ packer/scripts/webapp.sh
@@ -73,4 +73,3 @@ echo "+-------------------------------------------------------------+"
 sudo systemctl start webapp.service
 sudo systemctl status webapp.service
 sudo systemctl enable webapp.service
-sleep 30m
