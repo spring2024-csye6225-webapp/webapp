5d07e13a34942ad3057eaafc2aeed6909663ba0b
abhaydee <abhaygirish1997@gmail.com>
2024-02-23 14:44:19 -0500

    changes to delayss

--- packer/scripts/webapp.sh
+++ packer/scripts/webapp.sh
@@ -36,9 +36,7 @@ sudo -u postgres psql -U postgres -c "CREATE USER abhaydeshpande WITH PASSWORD '
 sudo -u postgres psql -U postgres -c "CREATE DATABASE cloudusers;"
 sudo -u postgres psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE cloudusers to abhaydeshpande;"
 
-
 sudo sed -i 's/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            password/g' /var/lib/pgsql/data/pg_hba.conf
-
 sudo sed -i 's/host    all             all             ::1\/128                 ident/host    all             all             ::1\/128                 password/g' /var/lib/pgsql/data/pg_hba.conf
 
 echo "Node.js and npm Installation"
@@ -49,6 +47,12 @@ echo "Node.js and npm Versions"
 node -v
 npm -v
 
+# Extract the zip file
+cd /tmp && unzip -q webapp-new.zip
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to unzip webapp-new.zip"
+    exit 1
+fi
 
 # Install unzip
 sudo yum install -y unzip
