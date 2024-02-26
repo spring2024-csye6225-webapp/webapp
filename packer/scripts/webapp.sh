1bbe9ee689ed2ccd02c6bdcc92a68f61b524e809
Abhay Deshpande <Deshpande.abh@northeastern.edu>
2024-02-25 19:18:45 -0500

    Feature/webapp packer (#43)
    * changes to packer features
    
    * changes to packer modules and scripts
    
    * commiting packer changes
    
    * removing files
    
    * changes to dir
    
    * removing sleep
    
    * changes to creds
    
    * packer changes for actions
    
    * changing dev cloud 2 to 0
    
    * changes to zone
    
    * sleep
    
    * changes to pwd
    
    * fixing scripts
    
    * fixing permissions
    
    * changes to packer config
    
    * chagnes to zip
    
    * adding space
    
    * changing script
    
    * changes to packer build
    
    * changes to packer build hcl
    
    * removing sleep
    
    * changes to delayss
    
    * fixing sleep
    
    * adding sleept
    
    * fixing shit
    
    * moving files properly
    
    * last try
    
    * fix scripts
    
    * fix scripts merge sleep

--- packer/scripts/webapp.sh
+++ packer/scripts/webapp.sh
@@ -27,6 +27,7 @@ sudo yum install unzip -y
 
 # Extract the zip file
 sudo mkdir -p /opt/csye6225/webapp-new
+sleep 10
 sudo unzip -q /tmp/webapp-new.zip -d /opt/csye6225/webapp-new/
 if [ $? -ne 0 ]; then
     echo "Error: Failed to unzip webapp-new.zip"
