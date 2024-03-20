5f67abdc01d8d221ed0b198b0dd7a5ad16d68984
Abhay Deshpande <Deshpande.abh@northeastern.edu>
2024-03-19 23:34:55 -0400

    Feature/webapp packer (#74)
    * changes to packer features
    
    * changes to packer modules and scripts
    
    * removing files
    
    * removing sleep
    
    * changes to creds
    
    * packer changes for actions
    
    * changing dev cloud 2 to 0
    
    * changes to zone
    
    * changes to packer config
    
    * removing sleep
    
    * changes to delayss
    
    * fix scripts
    
    * fix scripts merge sleep
    
    * sleep
    
    * CHANGES TO SCRIPTS
    
    * fix permissions
    
    * adding sleep for debug
    
    * adding sleep for debug
    
    * final merge error
    
    * format change
    
    * syntax mistake
    
    * changes to mistake
    
    * mistake error
    
    * changes to packer
    
    * changes to pcker
    
    * changes to readme
    
    * changes to cloud sql instance
    
    * resolving timeout error
    
    * omitting env
    
    * changes to scripts
    
    * giving back permission
    
    * changes to web logs
    
    * changes to agents
    
    * adding sleep
    
    * changes to scripts
    
    * removing sleep
    
    * adding sleep to check for errors
    
    * adding logs config
    
    * scripts changes for logger file
    
    * removing older script
    
    * adding ops agent

--- packer/scripts/webapp.sh
+++ packer/scripts/webapp.sh
@@ -24,12 +24,12 @@ echo "Node.js and npm Versions"
 node -v
 npm -v
 
-curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh
-sudo bash add-monitoring-agent-repo.sh --also-install
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 sudo mkdir -p /etc/google-cloud-ops-agent
 
 sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
-sudo systemctl restart google-cloud-ops-agent
+sudo systemctl start google-cloud-ops-agent
 
 # Install unzip
 sudo yum install -y unzip
