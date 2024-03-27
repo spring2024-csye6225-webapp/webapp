bb148597d166bebe05b619dfa5c3ec55a18692d1
abhaydee <abhaygirish1997@gmail.com>
2024-03-26 22:45:12 -0400

    fixing ie tests

--- test/integration.test.js
+++ test/integration.test.js
@@ -32,67 +32,69 @@ describe("test healthz api", () => {
   });
 });
 
-describe("USER API", function () {
-  it("should create a new user", async function () {
-    const responsePromise = await supertest(app).post("/v1/user").send({
-      first_name: "John",
-      last_name: "Doe",
-      username: "johndoe@example.com",
-      password: "password123",
-      userVerified: false,
-    });
-
-    const response = await responsePromise;
-    assert.strictEqual(response.status, 201);
-
-    const createdUser = await users.findOne({
-      where: { email: "johndoe@example.com" },
-    });
-    console.log("the created user", createdUser);
-    assert.ok(createdUser);
-    assert.strictEqual(createdUser.firstname, "John");
-    assert.strictEqual(createdUser.lastname, "Doe");
-    assert.strictEqual(createdUser.email, "johndoe@example.com");
-  });
-
-  if (process.env.NODE_ENV !== "dev") {
-    it("should update user information", async () => {
-      // To test the update functionality, we will simulate user verification here
-      const user = await users.create({
-        firstname: "John",
-        lastname: "Doe",
-        email: "johndoe@example.com",
+if (process.env.NODE_ENV !== "dev") {
+  describe("USER API", function () {
+    it("should create a new user", async function () {
+      const responsePromise = await supertest(app).post("/v1/user").send({
+        first_name: "John",
+        last_name: "Doe",
+        username: "johndoe@example.com",
         password: "password123",
+        userVerified: false,
       });
 
-      // Simulate user verification by updating the userVerified field to true
-      await user.update({ userVerified: true });
+      const response = await responsePromise;
+      assert.strictEqual(response.status, 201);
 
-      // Update user information
-      const response = await supertest(app)
-        .put("/v1/user/self")
-        .set(
-          "Authorization",
-          `Basic ${Buffer.from("johndoe@example.com:password123").toString(
-            "base64"
-          )}`
-        )
-        .send({
-          first_name: "Updated John",
-          last_name: "Updated Doe",
-          password: "updatedpassword123",
-        });
-
-      assert.strictEqual(response.status, 200);
-
-      // Check updated user information
-      const updatedUser = await users.findOne({
+      const createdUser = await users.findOne({
         where: { email: "johndoe@example.com" },
       });
-
-      assert.strictEqual(updatedUser.firstname, "Updated John");
-      assert.strictEqual(updatedUser.lastname, "Updated Doe");
-      assert.strictEqual(updatedUser.userVerified, true); // User should be verified after update
+      console.log("the created user", createdUser);
+      assert.ok(createdUser);
+      assert.strictEqual(createdUser.firstname, "John");
+      assert.strictEqual(createdUser.lastname, "Doe");
+      assert.strictEqual(createdUser.email, "johndoe@example.com");
     });
-  }
-});
+
+    if (process.env.NODE_ENV !== "dev") {
+      it("should update user information", async () => {
+        // To test the update functionality, we will simulate user verification here
+        const user = await users.create({
+          firstname: "John",
+          lastname: "Doe",
+          email: "johndoe@example.com",
+          password: "password123",
+        });
+
+        // Simulate user verification by updating the userVerified field to true
+        await user.update({ userVerified: true });
+
+        // Update user information
+        const response = await supertest(app)
+          .put("/v1/user/self")
+          .set(
+            "Authorization",
+            `Basic ${Buffer.from("johndoe@example.com:password123").toString(
+              "base64"
+            )}`
+          )
+          .send({
+            first_name: "Updated John",
+            last_name: "Updated Doe",
+            password: "updatedpassword123",
+          });
+
+        assert.strictEqual(response.status, 200);
+
+        // Check updated user information
+        const updatedUser = await users.findOne({
+          where: { email: "johndoe@example.com" },
+        });
+
+        assert.strictEqual(updatedUser.firstname, "Updated John");
+        assert.strictEqual(updatedUser.lastname, "Updated Doe");
+        assert.strictEqual(updatedUser.userVerified, true); // User should be verified after update
+      });
+    }
+  });
+}
